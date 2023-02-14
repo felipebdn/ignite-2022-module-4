a770b45582c1e6db5874bf13abf432b5f21f65d6
Felipe Barbosa <71861370+felipebdn@users.noreply.github.com>
2023-02-14 00:39:56 -0300

    123914022023

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
+
+import camiseta1 from '../assets/Shirt/1.png'
+import camiseta2 from '../assets/Shirt/2.png'
+// import camiseta3 from '../assets/Shirt/3.png'
+
 export default function Home() {
-  return <>Hello world</>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
